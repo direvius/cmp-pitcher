3c371fdd8d2806ae1fe51a88704436a6a46df126
Alexey Lavrenuke <direvius@gmail.com>
2012-08-07 15:42:23 +0400

    logger for app

--- src/main/java/ru/direvius/cmppitcher/App.java
+++ src/main/java/ru/direvius/cmppitcher/App.java
@@ -1,7 +1,7 @@
 package ru.direvius.cmppitcher;
 
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import ru.direvius.pitcher.Pitcher;
 
 /**
@@ -9,16 +9,17 @@ import ru.direvius.pitcher.Pitcher;
  *
  */
 public class App {
-
+    public static final Logger logger = LoggerFactory.getLogger(App.class);
     public static void main(String[] args) {
         int time = Integer.parseInt(args[0]);
         int rps = Integer.parseInt(args[1]);
+        logger.info("time: {}, rps: {}", time, rps);
         Pitcher p = new Pitcher(new UC1Ball());
         p.begin(rps);
         try {
             Thread.sleep(time * 1000);
         } catch (InterruptedException ex) {
-            Logger.getLogger(ru.direvius.pitcher.App.class.getName()).log(Level.SEVERE, null, ex);
+            logger.error("Interrupted", ex);
         }
         p.finish();
         System.exit(0);
